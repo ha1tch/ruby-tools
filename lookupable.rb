65fb087c7ed0e1e3459e24b702cf75e2898af2f5
vruz <vruz@users.noreply.github.com>
2017-02-07 08:09:04 -0300

    Update lookupable.rb

--- lookupable.rb
+++ lookupable.rb
@@ -24,8 +24,12 @@ a = [ ["Mercury", "Hg", 80, 200.592 ],
       ["Argon",   "Ar", 18,  39.948 ],
       ["Chromium","Cr", 24,  51.9961] ] 
 
-a.lookup(1,"Ar",3)  # returns atomic weight of Argon
+a.lookup( 1, "Ar", 3 )  # returns atomic weight of Argon
 
 h = {  "Hg" => { name: "Mercury",  atomicno: 80, weight: 200.592 },
        "Ar" => { name: "Argon",    atomicno: 18, weight:  39.948 },
        "Cr" => { name: "Chromium", atomicno: 24, weight:  51.9961}  }
+
+# return name of the element whose atomic number is 18
+h.lookup( :atomicno, 18, :name )
+    
